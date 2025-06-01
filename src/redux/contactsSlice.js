35fa673354f117f29ac3b820f7e347fd55866908
hanna-uler <hannauler@gmail.com>
2025-05-31 23:24:14 -0400

    refactor slices

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,26 +1,20 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    items: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-} 
-
-export const addContacts = createAction("contacts/addContact");
-export const deleteContact = createAction("contacts/deleteContact");
-
-
-export default function contactsReducer(state = initialState, action) {
-    switch (action.type) {
-        case "contacts/addContact":
-            return [...state.items, action.payload];
-        case "contacts/deleteContact":
-            return state.items.filter((item) => item.id !== action.payload);
-    
-        default:
-            return state;
+const contactsSlice = createSlice({
+    name: "contacts",
+    initialState: {
+        items: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ]
+    },
+    reducers: {
+        addContact: (state, action) => {[...state.items, action.payload]},
+        deleteContact: (state, action) => {state.items.filter((item) => item.id !== action.payload)}
     }
-}
+})
+
+export default contactsSlice.reducer;
+export const { addContact, deleteContact } = contactsSlice.actions;
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,15 +1,14 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    name: "",
-} 
-
-export const changeFilter = createAction("filters/changeFilter");
-export default function filtersReducer(state = initialState, action) {
-    switch (action.type) {
-        case "filters/changeFilter":
-            return action.payload;
-        default:
-            return state;
+const filtersSlice = createSlice({
+    name: "filters",
+    initialState: {
+        name: ""
+    },
+    reducers: {
+        changeFilter: (state, action) => {state.name = action.payload},
     }
-}
+})
+
+export default filtersSlice.reducer;
+export const {changeFilter} = filtersSlice.actions;

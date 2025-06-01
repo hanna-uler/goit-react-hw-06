87f2b3566999deca64ebe5606b1451dc245705dd
hanna-uler <hannauler@gmail.com>
2025-06-01 00:43:36 -0400

    set delete and render contacts with redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,39 @@
-import { useState, useMemo, useEffect } from 'react'
+// import { useState, useMemo, useEffect } from 'react'
 import ContactList from '../ContactList/ContactList'
 import ContactForm from '../ContactForm/ContactForm'
 import SearchBox from '../SearchBox/SearchBox'
-import { useDebounce } from 'use-debounce';
+// import { useDebounce } from 'use-debounce';
 import css from './App.module.css'
-
-const initContacts = [
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]
   
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    let savedContacts = window.localStorage.getItem("saved-contacts");
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    } else {
-      return initContacts;
-    }
-  })
-  const [searchQuery, setSearchQuery] = useState("");
-  const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
-  
-  const deleteContact = (id) => {
-    setContacts(contacts => contacts.filter((contact) => contact.id !== id))
-  }
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts))
-  }, [contacts]);
-  
-  const visibleContacts = useMemo(() => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(debouncedSearchQuery.toLowerCase()))
-  }, [debouncedSearchQuery, contacts]);
-
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm/>
       <SearchBox/>
-      <ContactList deleteContact={deleteContact} contacts={visibleContacts} />
+      <ContactList/>
     </div>
-
   )
+  // const [contacts, setContacts] = useState(() => {
+  //   let savedContacts = window.localStorage.getItem("saved-contacts");
+  //   if (savedContacts !== null) {
+  //     return JSON.parse(savedContacts);
+  //   } else {
+  //     return initContacts;
+  //   }
+  // })
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
+  
+ 
+
+  // useEffect(() => {
+  //   window.localStorage.setItem("saved-contacts", JSON.stringify(contacts))
+  // }, [contacts]);
+  
+  // const visibleContacts = useMemo(() => {
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(debouncedSearchQuery.toLowerCase()))
+  // }, [debouncedSearchQuery, contacts]);
+
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,60 +1,17 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css'
+import { useSelector } from 'react-redux';
 
-export default function ContactList({ contacts, deleteContact }) {
+export default function ContactList() {
+    const contacts = useSelector((state) => state.contacts.items);
     return (
         <ul className={css.container}>
             {contacts.map((contact) => {
                 return <li className={css.contactItem}
                     key={contact.id}>
-                    <Contact deleteContact={deleteContact} name={contact.name} number={contact.number} id={contact.id} />
+                    <Contact name={contact.name} number={contact.number} id={contact.id} />
                 </li>;
             })}
         </ul>
     )
 }
-
-// we need contacts array here
-// do we need query here?..
-// + deleting action
-
-
-// goit example with tasks:
-
-// // src/components/TaskList/TaskList.js
-
-// // 1. Імпортуємо хук
-// import { useSelector } from 'react-redux';
-// import { Task } from '../Task/Task';
-
-// const getVisibleTasks = (tasks, statusFilter) => {
-//   switch (statusFilter) {
-//     case 'active':
-//       return tasks.filter((task) => !task.completed);
-//     case 'completed':
-//       return tasks.filter((task) => task.completed);
-//     default:
-//       return tasks;
-//   }
-// };
-
-// export const TaskList = () => {
-// 	// 2. Отримуємо масив завдань із стану Redux
-//   const tasks = useSelector((state) => state.tasks.items);
-
-//   // 3. Отримуємо значення фільтра із стану Redux
-//   const statusFilter = useSelector((state) => state.filters.status);
-
-//   // 4. Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
-//   const visibleTasks = getVisibleTasks(tasks, statusFilter);
-
-//   return (
-//     <ul className={css.list}>
-//       {visibleTasks.map((task) => (
-//         <li className={css.listItem} key={task.id}>
-//           <Task task={task} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };

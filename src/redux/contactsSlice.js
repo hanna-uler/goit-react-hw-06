40e92ee5c99ceef6c956a99176d46a0470e64e02
hanna-uler <hannauler@gmail.com>
2025-06-01 10:36:24 -0400

    fix deleteContact reducer

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -15,11 +15,7 @@ const contactsSlice = createSlice({
             state.items.push(action.payload);
         },
         deleteContact: (state, action) => {
-            return {
-                ...state,
-                items: state.items.filter((item) => item.id !== action.payload)
-                // state.items.filter((item) => item.id !== action.payload);
-            }
+            state.items = state.items.filter((item) => item.id !== action.payload)
         }
     }
     })
